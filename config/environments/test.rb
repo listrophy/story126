c6be3cb7fdc582e0baee1885ab788b03bf6fbba9
Stephen Anderson <stephen@bendycode.com>
2009-03-07 17:23:43 -0600

    Rspec Setup.  Froze many gems into vendor/gems. Might want to replace with submodules later.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -20,3 +20,8 @@ config.action_controller.allow_forgery_protection    = false
 # The :test delivery method accumulates sent emails in the
 # ActionMailer::Base.deliveries array.
 config.action_mailer.delivery_method = :test
+
+config.gem "rspec", :lib => false, :version => ">= 1.1.99.12" 
+config.gem "rspec-rails", :lib => false, :version => ">= 1.1.99.12" 
+# nokogiri
+# webrat
