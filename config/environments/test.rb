34c62e407da498b5ba1ea53d26dc120b6e8c0536
Stephen Anderson <stephen@bendycode.com>
2009-03-07 18:15:55 -0600

    Froze cucumber and webrat into vendor gems.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -23,5 +23,6 @@ config.action_mailer.delivery_method = :test
 
 config.gem "rspec", :lib => false, :version => ">= 1.1.99.12" 
 config.gem "rspec-rails", :lib => false, :version => ">= 1.1.99.12" 
+config.gem 'webrat', :version => '>= 0.4.2'
 # nokogiri
 # webrat
